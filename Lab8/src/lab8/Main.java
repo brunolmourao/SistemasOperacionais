40fe1c125c192596bf95bd8ea5cffb23a1a4673f
Bruno Mourao <brunolmourao10@gmail.com>
2016-11-19 16:49:10 -0300

    Lab 8 Finalizado

--- Lab8/src/lab8/Main.java
+++ Lab8/src/lab8/Main.java
@@ -1,5 +1,7 @@
 package lab8;
 
+import java.util.Scanner;
+
 public class Main {
 
 	private static int TamanhoDoFrame = 3;
@@ -9,7 +11,27 @@ public class Main {
 		int[] Entradas;
 		String Entrada = "1,2,3,4,1,2,5,1,2,3,4,5";
 		Entradas = tc.processar(Entrada);
-		alg.Otimo(Entradas, TamanhoDoFrame);
+		String algoritmo;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira o nome do algoritmo a ser executado");
+		algoritmo = sc.next();
+		if(algoritmo.equals("FIFO")){
+			alg.FIFO(Entradas, TamanhoDoFrame);
+		}
+		if(algoritmo.equals("LRU")){
+			alg.LRU(Entradas, TamanhoDoFrame);
+		}
+		if(algoritmo.equals("MFU")){
+			alg.MFU(Entradas, TamanhoDoFrame);
+		}
+		if(algoritmo.equals("LFU")){
+			alg.LFU(Entradas, TamanhoDoFrame);
+		}
+		if(algoritmo.equals("Otimo")){
+			alg.Otimo(Entradas, TamanhoDoFrame);
+		}
+		
+		
 
 	}
 
